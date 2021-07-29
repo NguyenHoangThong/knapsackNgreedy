421d249f4d53a0def8f084376d7cd517adde9a8d
thong nguyen hoang <nhthong161195@gmail.com>
2021-07-29 16:58:22 +0700

    add write report to file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import json
 class Item(object):
     def __init__(self, value, count):
         self.value = value
@@ -53,22 +55,45 @@ def knapsack(packs, amount_send):
         i += 1
         if i > len(value) - 1:
             stop = True
-        print("result knapsack", total_amount)
-        print("total address used", count_address)
+    # print("result knapsack", total_amount)
+    # print("total address used", count_address)
+    return total_amount, count_address
 
 
 if __name__ == "__main__":
-    amount_send = 5
-    value = [1, 5, 8, 15, 19, 25]
-    count = [10, 20, 7, 12, 10, 1]
+    amount_send = 500
+    value = [round(random.uniform(0.001, 10), 10) for _ in range(100)]
+    # print(value)
+    # value = [1, 5, 8, 15, 19, 25]
+    # total of address in cluster
+    # count = [10, 20, 7, 12, 10, 1]
+    count = [random.randint(1, 100) for _ in range(100)]
+    print(count)
     packs = []
     for i in range(len(value)):
         packs.append(Item(value[i], count[i]))
-
     packs.sort(reverse=True)
-    div_inputs = greedy(packs, amount_send)
-    if len(div_inputs) > 1:
-        knapsack(div_inputs, amount_send)
-    else:
-        print("result greedy", div_inputs[0].value)
-        print("total address used", 1)
+    f = open("btc_report.txt", "w")
+    f.write("Input data \n")
+    f.write('Amount cluster: ')
+    json.dump(value, f)
+    f.write("\n")
+    f.write('Address in cluster: ')
+    json.dump(count, f)
+    f.write("\nTotal address {}".format(sum(count)))
+    f.write('\n\n\nOutputs')
+    for i in range(100):
+        f.write("\n ============================================================== \n")
+        amount_send = round(random.uniform(0.01, 10000), 10)
+        f.write("Amount send: ")
+        f.write(str(amount_send))
+        f.write("\n")
+        div_inputs = greedy(packs, amount_send)
+        if len(div_inputs) > 1:
+            total_send, count_address = knapsack(div_inputs, amount_send)
+            f.write("Knapsack\n")
+            f.write("Total send {} \nTotal address used {} \n".format(total_send, count_address))
+        else:
+            f.write("Greedy\n")
+            f.write("Total send {} \nTotal address used {} \n".format(div_inputs[0].value, 1))
+    f.close()
